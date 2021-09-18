8f67261feb0c7e05113f4d7094cbe18642a7b235
Richard Cao <richacao@amazon.com>
2021-09-17 18:55:06 -0700

    Added graceful shutdown for coinbasepro server. Reference: https://github.com/grpc/grpc/pull/26622

--- coinbasepro/server/server.py
+++ coinbasepro/server/server.py
@@ -27,6 +27,24 @@ class CoinbasePro(coinbasepro_pb2_grpc.CoinbaseProServicer):
             volume = str(response['volume']),
             time = str(response['time'])
         )
+
+    def Websocket(self, request, context):
+        websocket_params = {
+            'type': request.type,
+            'product_ids': [pid.product_id for pid in request.product_ids],
+            'channels': [{
+                'name': ch.name,
+                'channels': [pid.product_id for pid in ch.product_ids]
+            } for ch in request.channels]
+        }
+
+        with websockets.connect('wss://ws-feed.pro.coinbase.com') as websocket:
+            websocket.send(json.dumps(websocket_params))
+            while True:
+                response = websocket.recv()
+                yield coinbasepro_pb2.WebsocketResponse(
+                    type='hello'
+                )
     # https://stackoverflow.com/questions/53898185/how-can-i-use-grpc-with-asyncio
     # async def Websocket(self, request, context):
     #     websocket_params = {
@@ -51,13 +69,29 @@ class CoinbasePro(coinbasepro_pb2_grpc.CoinbaseProServicer):
         #             type='hello'
         #         )
 
+# PR for gRPC graceful shutdown: https://github.com/grpc/grpc/pull/26622
 async def serve():
     server = aio.server()
     coinbasepro_pb2_grpc.add_CoinbaseProServicer_to_server(CoinbasePro(), server)
     server.add_insecure_port('[::]:50051')
     await server.start()
+    async def server_graceful_shutdown():
+        logging.info("Starting graceful shutdown...")
+        # Shuts down the server with 0 seconds of grace period. During the
+        # grace period, the server won't accept new connections and allow
+        # existing RPCs to continue within the grace period.
+        await server.stop(5)
+    _cleanup_coroutines.append(server_graceful_shutdown())
     await server.wait_for_termination()
+
+# Coroutines to be invoked when the event loop is shutting down.
+_cleanup_coroutines = []
 
 if __name__ == '__main__':
     logging.basicConfig()
-    asyncio.run(serve())
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(serve())
+    finally:
+        loop.run_until_complete(*_cleanup_coroutines)
+        loop.close()
